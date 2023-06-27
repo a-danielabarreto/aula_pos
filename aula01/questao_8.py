0c08b049065c95ccdca4432a2be2a42e207654eb
Daniela <danielabna@gmail.com>
2023-06-27 20:22:42 -0300

    Revert "Add files via upload"
    This reverts commit ab7230f51e6e6fae38b6551b2093799bf7791c65.

--- aula01/questao_8.py
+++
@@ -1,9 +0,0 @@
-""" 8. Exercício da Conversão de Moedas: Escreva um programa em Python que solicite ao usuário um valor em Real (BRL) e
-faça a conversão desse valor para Dólar Americano (USD). Considere a taxa de câmbio fixa de 1 USD = 5 BRL. Exiba o
-valor convertido na tela. """
-
-real = float(input('Digite o valor em Real (BRL): '))
-
-dolar = real / 5
-
-print('O valor em dolar americano é: USD %.2f' %dolar)
--- aula02/exemplo3.py
+++
@@ -1,9 +0,0 @@
-""" Um programa que verifique se existe permissão, a
-partir da idade, para entrar em uma festa. """
-
-idade = int(input("Digite a sua idade: "))
-
-if idade >= 18:
-    print("Pode entrar na festa!")
-
-print('--- Fim do programa ---')
